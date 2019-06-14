085639e2c47742af473f5cadbfe9938d327911ed
Clement ORMAUX <clement.ormaux@gmail.com>
2019-06-14 14:14:40 +0200

    Merge et Insert

--- src/main/java/dev/jpa/mvn/App.java
+++ src/main/java/dev/jpa/mvn/App.java
@@ -1,9 +1,12 @@
 package dev.jpa.mvn;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import dev.jpa.mvn.domain.Address;
 import dev.jpa.mvn.domain.Contact;
 
 public class App {
@@ -13,25 +16,49 @@ public class App {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("digi-book-jpa");
 		EntityManager em = emf.createEntityManager();
 
+		// --------------------------INSERT INTO-------------------------------\\
 		// CREATION OU INSERT INTO
 		em.getTransaction().begin();
 
-		Contact contact = em.find(Contact.class, 4l);
+		Contact contact = em.find(Contact.class, 5l);
 		System.out.println(contact.getEmail());
-		em.remove(contact);
+		contact.setEmail("toto@titi.fr");
+		em.persist(contact);
 
 		// INTEGRER A LA BASE
 		em.getTransaction().commit();
 
-//		List<Contact> list = em.createQuery("from Contact", Contact.class).getResultList();
-//		for (Contact c : list) {
-//			System.out.println(c);
-//		}
-//		System.out.println();
-//		List<Address> listA = em.createQuery("from Address", Address.class).getResultList();
-//		for (Address a : listA) {
-//			System.out.println(a);
-//		}
+		// --------------------------UPDATE-------------------------------\\
+		// CREATION OU INSERT INTO
+//		em.getTransaction().begin();
+//
+//		Contact contact = em.find(Contact.class, 5l);
+//		System.out.println(contact.getEmail());
+//		contact.setEmail("totomod@titi.fr");
+//		em.merge(contact);
+
+		// INTEGRER A LA BASE
+//		em.getTransaction().commit();
+
+		// --------------------------DELETE-------------------------------\\
+		// CREATION OU INSERT INTO
+//		em.getTransaction().begin();
+//
+//		Contact contact = em.find(Contact.class, 5l);
+//		em.remove(contact);
+
+		// INTEGRER A LA BASE
+//		em.getTransaction().commit();
+
+		List<Contact> list = em.createQuery("from Contact", Contact.class).getResultList();
+		for (Contact c : list) {
+			System.out.println(c);
+		}
+		System.out.println();
+		List<Address> listA = em.createQuery("from Address", Address.class).getResultList();
+		for (Address a : listA) {
+			System.out.println(a);
+		}
 
 		em.close();
 		emf.close();
